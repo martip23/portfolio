997e652c9d2ed4e2ea60e625f55ccc28ec260aed
Patrick Lee <patrickmartinez77@gmail.com>
2022-02-15 21:36:10 -0600

    Circleci project setup (#1)
    * Add .circleci/config.yml
    
    * Add build and push to ECR step for CI
    
    * Add pull-and-run-from-ecr step
    
    * Testing CI Deployment

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Under Construction
+          TESTING CI DEPLOYMENT
         </p>
         <a
           className="App-link"
